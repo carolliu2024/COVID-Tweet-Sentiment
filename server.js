08e4eab36807d04fde5a29abd6ebcdf45606dcc6
carolliu2024 <CarolLiu2024@u.northwestern.edu>
2022-04-04 23:04:37 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 // Create server
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log('listening on 3000')
   });
 
