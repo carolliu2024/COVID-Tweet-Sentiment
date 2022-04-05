009e9f83151c08227111b8eab4a40ca9e59f57ba
carolliu2024 <CarolLiu2024@u.northwestern.edu>
2022-04-04 22:25:44 -0500

    map

---
+++ Map/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// Create server
+app.listen(3000, function() {
+    console.log('listening on 3000')
+  });
+
+// Make server send html page to browser
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/chloro_map_ex.html')
+  })
+
