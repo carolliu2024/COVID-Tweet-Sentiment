760a9f143ca0145f9791861c31909d49981a6092
carolliu2024 <CarolLiu2024@u.northwestern.edu>
2022-04-07 14:51:41 -0500

    Plotting each state's dominant emotion

--- Map/sentiment_map.py
+++ Map/sentiment_map.py
@@ -1,4 +1,5 @@
 # %%
+import antigravity
 import matplotlib.pyplot as plt
 import geopandas
 import pandas as pd
@@ -37,20 +38,26 @@ merged.head()
 # %% Matplotlib prep work for Mapping
 
 #  Set variables to map
-var = 'Total Covid Cases'
 
-# Create figure, axes for map to be drawn in
-#fig = plt.subplots(1, figsize = (10, 6))
+# Dominant emotions: Happy, angry, surprise, sad, fear
+happy = merged[merged['Dominant Emotion'] == 'Happy']
+angry = merged[merged['Dominant Emotion'] == 'Angry']
+surprise = merged[merged['Dominant Emotion'] == 'Surprise']
+sad = merged[merged['Dominant Emotion'] == 'Sad']
+fear = merged[merged['Dominant Emotion'] == 'Fear']
 
 #%% Create Map
-merged.explore(column = var,
-               cmap = 'plasma', 
-               scheme = 'EqualInterval') 
+
+sent_map = merged.explore()
+
+# merged.explore(column = var,
+#                cmap = 'plasma', 
+#                scheme = 'EqualInterval') 
 # ax.set_title('Total Covid Cases as of 3/10/22')
 
 # %%
 # Messy Colorbar labels 
-merged.explore(column = var,
-               cmap = 'plasma')
+# merged.explore(column = var,
+#                cmap = 'plasma')
 
 # %%
