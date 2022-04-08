9879ae4eb946f317939dc995d187bb4c36f32ade
carolliu2024 <CarolLiu2024@u.northwestern.edu>
2022-04-08 15:22:57 -0500

    Display map on browser

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+// Create server
+app.listen(process.env.PORT || 3000, function() {
+    console.log('listening on 3000')
+  });
+
+// Make server send html page to browser
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/Map/senti_map.html')
+  })
